c1dc78e8b5866352621901c65f8900603bdff66f
Adebola <bfagbowore@gmail.com>
2022-08-23 14:17:46 +0100

    update app url

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://merngoalappbola.herokuapp.com", //http://localhost:5000
+  // baseURL: "https://merngoalappbola.herokuapp.com",
+  baseURL: "http://localhost:5000",
 });
 
 const API_URL = "/api/users/";
